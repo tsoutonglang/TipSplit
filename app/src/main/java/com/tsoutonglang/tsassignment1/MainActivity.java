1597ce085586d3e181eea69fe62438fa5131f75a
Tania Soutonglang <tsoutonglang12@gmail.com>
2021-09-18 11:25:29 -0500

    formatted output to correct decimal place

--- app/src/main/java/com/tsoutonglang/tsassignment1/MainActivity.java
+++ app/src/main/java/com/tsoutonglang/tsassignment1/MainActivity.java
@@ -11,18 +11,17 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DecimalFormat;
 
+public class MainActivity extends AppCompatActivity {
     EditText billTotal;
 
     RadioGroup tipGroup;
     RadioButton tipButton;
-    TextView tipAmount;
-    TextView tipTotal;
+    TextView tipAmount, tipTotal;
 
     EditText peopleAmt;
-    TextView personTotal;
-    TextView overage;
+    TextView personTotal, overage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         billTotal = findViewById(R.id.editTextNumberDecimal);
+
         tipGroup = findViewById(R.id.radioGroupTip);
         tipAmount = findViewById(R.id.textTipAmountCalc);
         tipTotal = findViewById(R.id.textTipTotalCalc);
@@ -40,22 +40,39 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 int radioId = tipGroup.getCheckedRadioButtonId();
                 tipButton = findViewById(radioId);
-                double percent = Double.parseDouble(tipButton.getText().toString().substring(0, tipButton.getText().toString().indexOf('%')))/100.00;
-                Log.d("debug: ", Double.toString(percent));
-                double tip = calculateTip(percent);
-                tipAmount.setText(Double.toString(tip));
+
+                double bill;
+                if (billTotal.getText().toString().equals("")) {
+                    bill = 0.00;
+                    tipGroup.clearCheck();
+                } else {
+                    bill = Double.parseDouble(String.valueOf(billTotal.getText().toString()));
+                }
+                Log.d("debug - bill: ", Double.toString(bill));
+
+                double percent = Double.parseDouble(tipButton.getText().toString().substring(0, tipButton.getText().toString().indexOf('%')));
+                Log.d("debug - percent: ", Double.toString(percent));
+
+                double tip = calculateTip(percent, bill);
+                Log.d("debug - tip amount: ", Double.toString(tip));
+
+                double total = calculateTotal(tip, bill);
+                Log.d("debug - total w/ tip:  ", Double.toString(total));
+
+                tipAmount.setText("$" + String.format("%1$,.2f", tip));
+                tipTotal.setText("$" + String.format("%1$,.2f", total));
             }
         });
     }
-    public double calculateTip(double percent){
-        double bill = Double.parseDouble(billTotal.getText().toString());
-        double tip = bill*percent;
+
+    public double calculateTip(double percent, double bill){
+        double tip = bill*percent/100.00;
 
         return tip;
     }
 
-
-    public void calculateTotal(){
-
+    public double calculateTotal(double tip, double bill){
+        double total = bill+tip;
+        return total;
     }
 }
